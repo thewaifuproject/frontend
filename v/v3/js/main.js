74728bb7d2dd3abfcaa106a027d67708ff02f375
urlchange <salah.karim@e-campus.uab.cat>
2018-11-16 22:19:17 +0100

    landings

---
+++ v/v3/js/main.js
@@ -0,0 +1,21 @@
+/*function main(){
+    window.addEventListener("scroll",function(){
+        var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var body = document.body, html = document.documentElement;
+
+        var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+        
+        var secV = height/7
+        var redenH = ((scrollTop/height)/2)/(secV/height)
+        
+        var redenV = redenH*w
+        this.console.log(secV + "  " + height)
+        //var el = document.getElementById('d').style.width = "calc("+w+"px - "+redenV+"px)"transform: translate(-90%,0);
+        document.getElementById('b1').style.transform = "translate("+-(redenV/w)*100+"%, 0%)"
+        document.getElementById('b2').style.transform = "translate("+((-(redenV/w)*100)+100)+"%, 0%)"
+        document.getElementById('b3').style.transform = "translate("+((-(redenV/w)*100)+200)+"%, 0%)"
+        document.getElementById('b4').style.transform = "translate("+((-(redenV/w)*100)+300)+"%, 0%)"
+
+    });
+}*/
