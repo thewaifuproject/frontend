85d1f4b5ca8b6cf25be6467dccf1750e72d8dc1b
urlchange <salah.karim@e-campus.uab.cat>
2018-12-05 17:55:00 +0100

    mobile design fix

--- js/main.js
+++ js/main.js
@@ -6,39 +6,28 @@ function main(){
 
         var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
         
-        if (w<980)
-            var secV = height/11
+        if (w<=740)
+            var secV = height/14
         else
-            var secV = height/11
+            var secV = height/12
         var redenH = ((scrollTop/height)/2)/(secV/height)
         
         var redenV = redenH*w
-        //this.console.log(secV + "  " + height)
-        //var el = document.getElementById('d').style.width = "calc("+w+"px - "+redenV+"px)"transform: translate(-90%,0);
-        if (w<980){
-            document.getElementById('d1').style.transform = "translate("+-(redenV/w)*100+"%, 0%) scaleX(1.01)"
-            document.getElementById('b1').style.transform = "translate("+-(redenV/w)*100+"%, 0%) scaleX(1.01)"
-        }
-        else {
-            document.getElementById('d1').style.transform = "translate("+-(redenV/w)*100+"%, 0%) scaleX(1.01)"
-            document.getElementById('b1').style.transform = "translate("+-(redenV/w)*100+"%, 0%) scaleX(1.01)"
+
+        document.getElementById('d1').style.transform = "translate("+-(redenV/w)*100+"%, 0%) scaleX(1.01)"
+        document.getElementById('b1').style.transform = "translate("+-(redenV/w)*100+"%, 0%) scaleX(1.01)"
+        document.getElementById('d2').style.transform = "translate("+((-(redenV/w)*100)+100)+"%, 0%) scaleX(1.01)"
+        document.getElementById('d3').style.transform = "translate("+((-(redenV/w)*100)+150)+"%, 0%) scaleX(1.01)"
+        document.getElementById('d4').style.transform = "translate("+((-(redenV/w)*100)+250)+"%, 0%) scaleX(1.1)"
+        document.getElementById('d5').style.transform = "translate("+((-(redenV/w)*100)+350)+"%, 0%) scaleX(1.01)"
+        document.getElementById('d5-1').style.transform = "translate("+((-(redenV/w)*100)+450)+"%, 0%) scaleX(1.01)"
+        if (w<= 740) {
+            document.getElementById('d5-2').style.transform = "translate("+((-(redenV/w)*100)+550)+"%, 0%) scaleX(1.01)"
+            document.getElementById('d6').style.transform = "translate("+((-(redenV/w)*100)+650)+"%, 0%) scaleX(1.01)"
+        } else{
+            document.getElementById('d6').style.transform = "translate("+((-(redenV/w)*100)+550)+"%, 0%) scaleX(1.01)"
         }
 
-        //document.getElementById('b2').style.transform = "translate("+((-(redenV/w)*100)+100)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d2').style.transform = "translate("+((-(redenV/w)*100)+100)+"%, 0%) scaleX(1.01)"
-        //document.getElementById('db2').style.transform = "translate("+((-(redenV/w)*100)+100-1)+"%, 0%)"
-        //document.getElementById('b3').style.transform = "translate("+((-(redenV/w)*100)+200)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d3').style.transform = "translate("+((-(redenV/w)*100)+200)+"%, 0%) scaleX(1.01)"
-        //document.getElementById('b4').style.transform = "translate("+((-(redenV/w)*100)+300)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d4').style.transform = "translate("+((-(redenV/w)*100)+300)+"%, 0%) scaleX(1.1)"
-        //document.getElementById('b5').style.transform = "translate("+((-(redenV/w)*100)+400)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d5').style.transform = "translate("+((-(redenV/w)*100)+400)+"%, 0%) scaleX(1.01)"
-        //document.getElementById('b6').style.transform = "translate("+((-(redenV/w)*100)+500)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d6').style.transform = "translate("+((-(redenV/w)*100)+500)+"%, 0%) scaleX(1.01)"
-        /*document.getElementById('b7').style.transform = "translate("+((-(redenV/w)*100)+500)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d7').style.transform = "translate("+((-(redenV/w)*100)+500)+"%, 0%) scaleX(1.01)"
-        document.getElementById('b8').style.transform = "translate("+((-(redenV/w)*100)+500)+"%, 0%) scaleX(1.01)"
-        document.getElementById('d8').style.transform = "translate("+((-(redenV/w)*100)+500)+"%, 0%) scaleX(1.01)"*/
         
         //console.log(document.getElementById('test2').getBoundingClientRect().top)
         if (document.getElementById('test2').getBoundingClientRect().top > 0){
