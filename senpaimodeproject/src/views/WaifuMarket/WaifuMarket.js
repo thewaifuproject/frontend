82c62d4d0eba1a5745c1e6abda8b7afc41e0d133
Albert <github@albert.sh>
2019-01-06 01:20:54 +0100

    Fix layout bug Once rendered, the CSS in WaifuMarket.css would affect the other pages, bugging their layouts

--- senpaimodeproject/src/views/WaifuMarket/WaifuMarket.js
+++ senpaimodeproject/src/views/WaifuMarket/WaifuMarket.js
@@ -4,10 +4,18 @@ import './WaifuMarket.css'
 
 class WaifuMarket extends Component {
 
+	componentDidMount() {
+		document.querySelector(".main-container.container-fluid").classList.add("market");
+	}
+
     render() {
 
         return <iframe src='https://rinkeby.opensea.io/category/waifuchainv3?embed=true' width='100%' height='100%' frameborder='0' allowfullscreen></iframe>
     }
+
+	componentWillUnmount() {
+		document.querySelector(".main-container.container-fluid").classList.remove("market");
+	}
 
 }
 
